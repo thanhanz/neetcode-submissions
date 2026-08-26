929d6795f81ae1f0e1393d60c4b4b089c40adf96
Thanh An <thanhann.2934@gmail.com>
2026-08-27 00:11:50 +0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def getMaxValueInDict(self, map: {}) -> int:
+        maxKey = max(map, key=map.get)
+        maxVal = map[maxKey]
+        return maxVal
+
+    def characterReplacement(self, s: str, k: int) -> int:
+        left, maxLength, maxFreq = 0, 0, 0
+        freq = {}
+
+        for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
+
+            maxFreq = self.getMaxValueInDict(freq)
+
+            windowSize = right - left + 1
+
+            if (windowSize - maxFreq > k):
+                freq[s[left]] = freq.get(s[left]) - 1
+                left += 1
+
+            windowSize = right - left + 1
+            maxLength = max(maxLength, windowSize)
+        
+        return maxLength
