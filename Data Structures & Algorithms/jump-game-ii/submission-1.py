f0e1b2619f4d4646f9cd2b1ee6b9f9e896e27c1e
Thanh An <thanhann.2934@gmail.com>
2026-09-11 12:03:06 +0700

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        # Greedy - DP
+        # Time: O(n)
+        # Space: O(1)
+
+        far, end, smallest_jump = 0, 0, 0
+        for i in range(len(nums) - 1):
+            far = max(far, i + nums[i])
+
+            if i == end:
+                end = far
+                smallest_jump += 1
+
+        return smallest_jump
