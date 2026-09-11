c4b108308e8b7956f9f29fba4164210228b49c37
Thanh An <thanhann.2934@gmail.com>
2026-09-11 12:59:28 +0700

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        # sum(cost) > sum(gas): return -1
+        # find start posible index -> Remove Brute Force O(n^2) time complexity
+        # Greedy: find start index -> no need to recalculate again -> O(n) time complexity
+        if len(gas) != len(cost):
+            return -1
+
+        if sum(cost) > sum(gas):
+            return -1
+
+        cur_gas, start = 0, 0
+        for i in range(len(gas)):
+            cur_gas += gas[i] - cost[i]
+            if cur_gas < 0:
+                start = i + 1
+                cur_gas = 0
+
+        return start
