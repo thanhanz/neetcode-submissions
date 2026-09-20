cdd1ba2d0342c4c863d4dd05271b8c3b791bb130
Thanh An <thanhann.2934@gmail.com>
2026-09-20 09:18:56 +0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # The case Greedy (Amount -= largest num in array)
+        # Case: [1, 5, 6, 9] -> output = 2 (5 + 6), if Greedy it will return 3 (9 + 1 + 1)
+        
+        dp = [0] * (amount + 1)
+        coins.sort()
+        for i in range(1, amount + 1):
+            minCoin = sys.maxsize
+
+            for coin in coins:
+                if i < coin:
+                    break
+                minCoin = min(minCoin, 1 + dp[i - coin])
+            dp[i] = minCoin
+        
+        if dp[amount] == sys.maxsize:
+            return -1
+        return dp[-1]
+
+                
+            
