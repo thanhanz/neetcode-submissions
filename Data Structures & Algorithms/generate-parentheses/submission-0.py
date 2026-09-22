e5656eb409cb1ba91f913db07a36f9e15d2956fd
Thanh An <thanhann.2934@gmail.com>
2026-09-22 11:41:05 +0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        result = []
+        parentheses_options = "("
+        open = 1
+        close = 0
+        
+        def backTrack(parentheses_options, open, close, n, result):
+            if open < close or open > n:
+                return
+
+            backTrack(parentheses_options + "(", open + 1, close, n, result)
+
+            backTrack(parentheses_options + ")", open, close + 1, n, result)
+            
+            if len(parentheses_options) == n * 2:
+                result.append(parentheses_options)
+                return
+
+        backTrack(parentheses_options, open, close, n, result)
+        return result
