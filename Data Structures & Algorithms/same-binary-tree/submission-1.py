6e7ea55d7a903c2443a77c47591c486787744519
Thanh An <thanhann.2934@gmail.com>
2026-09-26 00:16:32 +0700

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+
+        if p is None or q is None:
+            return False
+        queue = [(p, q)]
+        
+        # queue pop(0) - append()
+        while queue:
+            p, q = queue.pop(0)
+
+            # if ((current[0].left is None and current[1].left is not None) 
+            #     or 
+            #     (current[0].left is not None and current[1].left is None)):
+            #     return False
+            
+            # if ((current[0].right is None and current[1].right is not None) 
+            #     or 
+            #     (current[0].right is not None and current[1].right is None)):
+            #     return False
+            
+            # if (current[0].left is not None and current[1].left is not None):
+            #     queue.append((current[0].left, current[1].left))
+
+            # if (current[0].right is not None and current[1].right is not None):
+            #     queue.append((current[0].right, current[1].right))
+
+            # if current[0].val != current[1].val:
+            #     return False
+            if not p and not q:
+                continue
+                
+            if not p or not q:
+                return False
+            
+            if p and q and p.val != q.val:
+                return False
+            
+            queue.append((p.left, q.left))
+            queue.append((p.right, q.right))
+        return True
+
